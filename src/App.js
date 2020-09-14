c84d6234bdfe51bcb4df29ae020960d81a939e2b
Jeff Sargent <jeff.sargent@gmail.com>
2020-09-14 15:40:33 -0400

    added intervals to the scales

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 var notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'Bb', 'B'];
 
 var scales = [
-    { id: "major", name: "Major Scale", pattern: [0, 2, 2, 1, 2, 2, 2, 1] },
-    { id: "natural_minor", name: "Natural Minor Scale", pattern: [0, 2, 1, 2, 2, 1, 2, 2] },
-    { id: "harmonic_minor", name: "Harmonic Minor Scale", pattern: [0, 2, 1, 2, 2, 1, 3, 1] },
-    { id: "melodic_minor", name: "Melodic Minor Scale", pattern: [0, 2, 1, 2, 2, 2, 2, 1]},
-    { id: "pentatonic_major", name: "Pentatonic Major Scale", pattern: [0, 2, 2, 3, 2, 3] },
-    { id: "pentatonic_minor", name: "Pentatonic Minor Scale", pattern: [0, 3, 2, 2, 3, 2] },
-    { id: "blues", name: "Blues Scale", pattern: [0, 3, 2, 1, 1, 3, 2] }
+    { id: "major", name: "Major Scale", pattern: [2, 2, 1, 2, 2, 2, 1], intervals: ["1", "2", "3", "4", "5", "6", "7", "8"] },
+    { id: "natural_minor", name: "Natural Minor Scale", pattern: [2, 1, 2, 2, 1, 2, 2], intervals: ["1", "2", "b3", "4", "5", "b6", "b7", "8"] },
+    { id: "harmonic_minor", name: "Harmonic Minor Scale", pattern: [2, 1, 2, 2, 1, 3, 1], intervals: ["1", "2", "b3", "4", "5", "b6", "7", "8"] },
+    { id: "melodic_minor", name: "Melodic Minor Scale", pattern: [2, 1, 2, 2, 2, 2, 1], intervals: ["1", "2", "b3", "4", "5", "6", "7", "8"] },
+    { id: "pentatonic_major", name: "Pentatonic Major Scale", pattern: [2, 2, 3, 2, 3], intervals: ["1", "2", "3", "5", "6", "8"] },
+    { id: "pentatonic_minor", name: "Pentatonic Minor Scale", pattern: [3, 2, 2, 3, 2], intervals: ["1", "b3", "4", "5", "b7", "8"] },
+    { id: "blues", name: "Blues Scale", pattern: [3, 2, 1, 1, 3, 2], intervals: ["1", "2", "b3", "4", "b5", "6", "b7", "8"] }
 ];
 
 class KeyMaster extends Component {
@@ -76,34 +76,35 @@ class KeyMaster extends Component {
 
 class Scale extends Component {
     
-    getNotes(rootNote, scalePattern) {
+    getNotes(rootNote, scale) {
         var scaleNotes = [];
+        var scalePattern = scale.pattern;
         var index = notes.indexOf(rootNote);
-        for (var i = 0; i < scalePattern.length; i++) {
-            index += scalePattern[i];
+        for (var i = 0; i <= scalePattern.length; i++) {
+            
             if (index >= notes.length) {
                 index -= notes.length;
             }
             scaleNotes.push(notes[index]);
+            index += scalePattern[i];
         }
         return scaleNotes;
     }
 
     render() {
-        let scaleNotes = this.getNotes(this.props.rootNote, this.props.scale.pattern);
+        let scaleNotes = this.getNotes(this.props.rootNote, this.props.scale);
         return (
             <div className="scale">
                 {scaleNotes.map((value, index) => {
                     return (
-                        <div className="notebox" key={index}>
-                            <strong>{index+1}</strong><br/>
+                        <div className="notebox">
+                            <strong>{this.props.scale.intervals[index]}</strong><br/>
                             <div className="note">{value}</div>
                         </div>
                     )
                 })}
             </div>
         )
-        
     }
 }
 
