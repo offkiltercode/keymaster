d7bdd90975bd79efa895272e7f6bf8347ebcf574
Jeff Sargent <jeff.sargent@gmail.com>
2020-09-18 16:58:00 -0400

    updated gitignore

---
+++ build/precache-manifest.3ce9ce9c440def4b8c5f82ee8ce6fae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580fcfcb8c58e0692332c8adcfac1c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8a6d30e756e8dd9673d",
+    "url": "/static/css/main.091ab6da.chunk.css"
+  },
+  {
+    "revision": "60af8f931dba44caa5c3",
+    "url": "/static/js/2.92de7c6a.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.92de7c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a6d30e756e8dd9673d",
+    "url": "/static/js/main.e994544c.chunk.js"
+  },
+  {
+    "revision": "e4cce178e3f881a7162f",
+    "url": "/static/js/runtime-main.2d05b04d.js"
+  }
+]);
